b2edb23d38cfc7741f03307a48c3c369cc4276b5
tianzeng <wyzjtian@163.com>
2018-05-09 16:02:34 +0800

    增加对图片的操作

--- application/index/controller/Upload.php
+++ application/index/controller/Upload.php
@@ -9,6 +9,7 @@
 namespace app\index\controller;
 
 use think\Request;
+use think\Image;
 
 
 //文件上传,P216
@@ -29,7 +30,7 @@ class Upload extends \think\Controller
             $this->error('请选择上传文件');
         }
 // 移动到框架应用根目录/public/uploads/ 目录下
-        $info = $file->validate(['ext'=>'jpg,png'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+        $info = $file->validate(['ext' => 'jpg,png'])->move(ROOT_PATH . 'public' . DS . 'uploads');
         if ($info) {
             $this->success('文件上传成功：' . $info->getRealPath());
         } else {
@@ -37,4 +38,47 @@ class Upload extends \think\Controller
             $this->error($file->getError());
         }
     }
+
+
+    // 图片上传处理
+    public function picture(Request $request)
+    {
+// 获取表单上传文件
+        $file = $request->file('image');
+        if (true !== $this->validate(['image' => $file], ['image' => 'require|image'])) {
+            $this->error('请选择图像文件');
+        } else {
+// 读取图片
+            $image = Image::open($file);
+// 图片处理
+            switch ($request->param('type')) {
+                case 1: // 图片裁剪
+                    $image->crop(300, 300);
+                    break;
+                case 2: // 缩略图
+                    $image->thumb(150, 150, Image::THUMB_CENTER);
+                    break;
+                case 3: // 垂直翻转
+                    $image->flip();
+                    break;
+                case 4: // 水平翻转
+                    $image->flip(Image::FLIP_Y);
+                    break;
+                case 5: // 图片旋转
+                    $image->rotate();
+                    break;
+                case 6: // 图片水印
+                    $image->water('./logo.png', Image::WATER_NORTHWEST, 50);
+                    break;
+                case 7: // 文字水印
+                    $image->text('ThinkPHP', VENDOR_PATH . 'topthink/think-captcha/assets/ttfs/1.ttf', 20, '#ffffff');
+                    break;
+            }
+// 保存图片（以当前时间戳）
+            $saveName = $request->time() . '.png';
+            $image->save(ROOT_PATH . 'public/uploads/' . $saveName);
+            $this->success('图片处理完毕...', '/uploads/' . $saveName, 1);
+        }
+    }
+
 }
